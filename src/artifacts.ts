228c2fcc24a08c7bb7302e4867b8a7815d49d028
Ashton Dong <dong.ashton@gmail.com>
2024-11-18 15:55:34 -0500

    REFACTOR: remove the majority of set and artifact code in preparation for a rewrite

--- src/artifacts.ts
+++ src/artifacts.ts
@@ -58,99 +58,14 @@ function find_eb_arti_stones(inventory : saveTypes.InventoryItemsList[]) {
  */
 // @ts-ignore: namespace doesn't matter
 function find_best_artifacts(artifact_list: saveTypes.InventoryItemsList[]): { [key: number]: { level: number, rarity: number, amount: number} } {
-    return artifact_list.reduce<{ [key: number]: { level: number, rarity: number, amount : number } }>((acc, artifact) => {
+    return artifact_list.reduce<{
+        [key: number]: { level: number, rarity: number, amount: number }
+    }>((acc, artifact) => {
         const arti_spec = artifact.artifact.spec;
         // If the artifact name is not in the accumulator or if the current level is higher, update the accumulator
         if (!acc[arti_spec.name] || arti_spec.level > acc[arti_spec.name].level) {
-            acc[arti_spec.name] = { level: arti_spec.level, rarity: arti_spec.rarity, amount: artifact.quantity };
+            acc[arti_spec.name] = {level: arti_spec.level, rarity: arti_spec.rarity, amount: artifact.quantity};
         }
         return acc;
     }, {}); // Initial value of the accumulator is an empty object
-}
-
-/**
- * calculates the actual se boosts
- * @param artifact_list the list of artifacts, should be the output from find_best_artifacts()
- * @returns a object containing soul and prop boosts values
- */
-function calculate_arti_boosts(artifact_list : {[key : number] : {level : number, rarity : number}}) : { prop_boost : number, soul_boost : number} {
-    let prop_boost = 0.0
-    let soul_boost = 0.0
-
-    Object.keys(artifact_list).forEach( key => {
-        let int_key = parseInt(key)
-        if (int_key === BOOK) {
-            prop_boost += BOOK_EFFECT[artifact_list[int_key]["level"]][artifact_list[int_key]["rarity"]]
-        }
-        if (int_key === PROP) {
-            prop_boost += PROP_EFFECT[artifact_list[int_key]["level"]][artifact_list[int_key]["rarity"]]
-        }
-        if (int_key === SOUL) {
-            soul_boost += SOUL_EFFECT[artifact_list[int_key]["level"]][artifact_list[int_key]["rarity"]]
-        }
-    })
-
-    return {prop_boost: prop_boost, soul_boost : soul_boost}
-}
-
-/**
- * finds a given artifact by its id number
- * @param artifact_inv the artifact inventory
- * @param id the id of the artifact to find
- * @returns saveTypes.InventoryItemList
- */
-function find_artifact_by_id(artifact_inv : saveTypes.InventoryItemsList[], id : number) {
-    return artifact_inv.find(artifact => id === artifact.itemId)
-}
-
-/**
- * determines if an artifact contains eb stones
- * @param arti the artifact
- */
-function contains_eb_stones(arti : saveTypes.Artifact) {
-    for (let i = 0; arti.stonesList.length; i++)  {
-        let stone_name = arti.stonesList[i].name
-        if (EB_ARTIFACT_IDS[stone_name]) {
-            return true
-        }
-    }
-    return false
-}
-
-/**
- * determines if an artifact affects eb
- * @param arti a Artifact item
- * @returns boolean true or false
- */
-function is_eb_artifact(arti : saveTypes.Artifact) {
-    return !!EB_ARTIFACT_IDS[arti.spec.name];
-}
-
-/**
- * finds all sets within the saved sets that contain artifacts 
- * @param set_list the list of saved artifact sets
- * @param inv the inventory
- * @returns a 2d array of artifacts
- */
-function find_eb_sets(set_list : saveTypes.SavedArtifactSetsList[], inv : saveTypes.InventoryItemsList[]) {
-    let eb_sets : saveTypes.Artifact[][] = []
-    for (let i = 0; i < set_list.length; i++) {
-        let arti_id_list = set_list[i].slotsList
-        let is_eb_set = false
-        let arti_list : saveTypes.Artifact[] = []
-        for (let j = 0; j < arti_id_list.length; j++) {
-            let arti = find_artifact_by_id(inv, arti_id_list[j].itemId).artifact
-            arti_list.push(arti)
-            if (is_eb_artifact(arti)) {
-                is_eb_set = true
-            } 
-            else if (contains_eb_stones(arti)) {
-                is_eb_set = true
-            }
-        }
-        if (is_eb_set) {
-            eb_sets.push(arti_list)
-        }
-    }
-    return eb_sets
 }
